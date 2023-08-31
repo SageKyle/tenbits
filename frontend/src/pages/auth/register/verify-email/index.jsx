db0f14a0699db47ad412e838c4ea2734650bea5b
SageKyle <ominyipaulo@gmail.com>
2023-08-31 20:51:03 +0100

    add success checkmark animation

--- frontend/src/pages/auth/register/verify-email/index.jsx
+++ frontend/src/pages/auth/register/verify-email/index.jsx
@@ -1,14 +1,18 @@
 import Footer from '@/components/Footer'
 import PrimaryBtn from '@/components/PrimaryBtn'
 import { TbSocial } from 'react-icons/tb'
+import checkmarkAnimation from './checkmarkStyles.module.css'
 
 const user = { firstName: 'John', lastName: 'Doe', email: 'example@email.com' }
 
 export default function VerifyEmail() {
 	return (
 		<article className="w-screen h-screen flex flex-col items-center justify-between">
-			<section className="h-fit w-[90%] bg-accent mt-8 bg-opacity-10 flex flex-col justify-center px-6 gap-4 max-w-[30rem] p-8 justify-self-center shadow-sm rounded-lg">
-				<TbSocial className="inline-block self-center text-2xl text-green-400" />
+			<section className="h-fit w-[90%] bg-accent mt-8 bg-opacity-20 flex flex-col justify-center px-6 gap-4 max-w-[30rem] p-8 justify-self-center shadow rounded-lg">
+				{/* animated success checkmark with /> */}
+				<div className={`${checkmarkAnimation.circle} mx-auto`}>
+					<span className={`${checkmarkAnimation.checkmark}`}></span>
+				</div>
 
 				<h2>
 					Congratulations{' '}
