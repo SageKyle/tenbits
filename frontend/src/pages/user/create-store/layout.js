76b340819d2a80baf0d672cb6edc7d4ba0591a30
SageKyle <ominyipaulo@gmail.com>
2023-09-07 02:10:18 +0100

    add store name and tagline to create-store process

--- frontend/src/pages/user/create-store/layout.js
+++ frontend/src/pages/user/create-store/layout.js
@@ -1,16 +1,34 @@
 import Link from 'next/link'
 
-export default function Layout({ children }) {
+export default function Layout({ children, completed }) {
+	function previousPage() {
+		window.history.back()
+	}
+
 	return (
-		<>
+		<section className="w-full min-h-screen flex flex-col items-center justify-center">
+			<section className="w-4/5 mx-auto my-6">
+				<div className="w-full h-4 flex rounded-full bg-accent mb-4">
+					<span
+						className={`${completed} inline-block rounded-full h-full bg-primary`}
+					></span>
+				</div>
+				<div
+					className="flex text-primary font-semibold cursor-pointer self-start"
+					onClick={previousPage}
+				>
+					<span className="[rotate:-90deg] font-cursive text-lg">^</span>
+					<span>Back</span>
+				</div>
+			</section>
 			<>{children}</>
 
-			<div className="mt-6 mx-auto w-fit">
+			<div className="mt-6 mx-auto w-fit p-4">
 				<span>Having difficulty setting up your e-Store? </span>
 				<Link href={'store/feedback'} className="text-secondary">
 					Contact our team
 				</Link>
 			</div>
-		</>
+		</section>
 	)
 }
