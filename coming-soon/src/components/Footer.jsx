5f81fc3d4c8a48a344893e9f57c2477177c7c6d3
SageKyle <ominyipaulo@gmail.com>
2023-09-12 00:42:23 +0100

    complete coming soon site

--- coming-soon/src/components/Footer.jsx
+++ coming-soon/src/components/Footer.jsx
@@ -6,20 +6,12 @@ export default function Footer() {
 		<footer className="w-full flex flex-col gap-4 p-6 items-center justify-center">
 			<section className="flex items-center justify-center gap-4 w-full p-4">
 				<a
-					href="https://twitter.com/@Ominyi_Saje"
+					href="https://twitter.com/@_tenbits"
 					target="_blank"
 					title="Twitter"
 					className="text-2xl hover:text-[#222566] transition-colors ease-in-out duration-200"
 				>
 					<FaTwitter />
-				</a>
-				<a
-					href="https://linkedin.com/in/ominyi"
-					target="_blank"
-					title="LinkedIn"
-					className="text-2xl hover:text-[#222566] transition-colors ease-in-out duration-200"
-				>
-					<FaLinkedin />
 				</a>
 				<a
 					href="mailto:e.tenbits@gmail.com"
--- coming-soon/src/components/Header.jsx
+++ coming-soon/src/components/Header.jsx
@@ -1,13 +1,28 @@
+import Logo from '@/assets/icons/tenbits-logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Header({ extraStyles, left }) {
 	return (
 		<header
 			className={`w-full h-12 p-8 flex items-center justify-center ${extraStyles}`}
 		>
-			<h1
-				className={`font-bold text-2xl lg:text-3xl ${left ? 'lg:mr-auto' : ''}`}
+			<Link
+				href={'/'}
+				className={` h-full w-fit text-2xl lg:text-3xl flex items-baseline gap-3 ${
+					left ? 'lg:mr-auto' : ''
+				}`}
 			>
-				TenBits e-Store
-			</h1>
+				<div className="relative h-10 w-32">
+					<Image
+						src={Logo}
+						priority
+						fill
+						className="w-fit h-full object-contain object-center"
+					/>
+				</div>
+				{/* <span>e-Store</span> */}
+			</Link>
 		</header>
 	)
 }
