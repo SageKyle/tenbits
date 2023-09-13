70907064227c21d83d5d18621edb6137f95761c0
SageKyle <ominyipaulo@gmail.com>
2023-09-13 12:04:43 +0100

    add logo input section of create-business user-flow

---
+++ frontend/src/pages/user/create-store/logo.jsx
@@ -0,0 +1,65 @@
+'use client'
+import PrimaryBtn from '@/components/PrimaryBtn'
+import Link from 'next/link'
+import { useState } from 'react'
+import { HiArrowTopRightOnSquare } from 'react-icons/hi2'
+import Layout from './layout'
+
+export default function Logo() {
+	const [imageSrc, setImageSrc] = useState(null)
+
+	return (
+		<Layout completed={'w-[40%]'}>
+			<article className="flex flex-col items-center justify-center gap-4 bg-white mx-auto w-full max-w-[30rem] p-8  shadow-md md:rounded-lg">
+				<h1 className="text-2xl font-bold capitalize text-primary-dark mb-2">
+					logo
+				</h1>
+				<p>
+					A logo is an image that represents a company. It should be sharp and
+					simple enough to make it memorable, just like the Apple logo.
+				</p>
+				<p className="w-full flex flex-col">
+					Don't have a logo?
+					<Link
+						href={'mailto:info.tenbits@gmail.com?subject="logo design"'}
+						className="text-primary font-semibold"
+					>
+						Contact out team to get a professional logo design.
+					</Link>
+					<a
+						href="https://logo.com"
+						target="_blank"
+						referrerPolicy="noreferrer noopener"
+						className="text-secondary flex items-center gap-1"
+					>
+						Click here to generate a logo for free <HiArrowTopRightOnSquare />
+					</a>
+				</p>
+				<form className="w-full flex flex-col gap-4 mt-3">
+					<label className="flex flex-col space-y-2 w-full">
+						<span class="sr-only">Choose profile photo</span>
+						<input
+							type="file"
+							accept="image/*"
+							placeholder="logo..."
+							required
+							name="tagline"
+							// onChange={handleChange}
+							// value={formData.email}
+							className="w-full rounded-md bg-accent  file:mr-4 file:py-3 file:px-5
+                            file:rounded-sm file:border-0
+                            file:text-sm file:font-semibold
+                            file:bg-primary file:text-accent
+                            hover:file:bg-primary-dark"
+						/>
+					</label>
+					<PrimaryBtn
+						styles="w-full mt-8 py-4 font-semibold text-lg"
+						link={'socials'}
+						text={'next'}
+					/>
+				</form>
+			</article>
+		</Layout>
+	)
+}
--- frontend/src/pages/user/create-store/tagline.jsx
+++ frontend/src/pages/user/create-store/tagline.jsx
@@ -34,7 +34,7 @@ export default function Tagline() {
 					</label>
 					<PrimaryBtn
 						styles="w-full mt-8 py-4 font-semibold text-lg"
-						link={'socials'}
+						link={'logo'}
 						text={'next'}
 					/>
 				</form>
